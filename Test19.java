329d270f65165e49fe3093cc97a9ca16205fb23c
blast66 <113650618+blast66@users.noreply.github.com>
2022-11-04 11:31:59 +0800

    Add files via upload

---
+++ Test19.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test19 {
+    public static void main(String[] args){
+        int a;
+        int k = 7;
+        a = add(k, 4);
+        System.out.printf("%d%n", a);
+        a = add(k, 4);
+        System.out.printf("%d%n", a);
+			}
+    public static int add(int x, int y) {
+        int t = add (x,y);
+        return t ;
+    }
+		}
+        
