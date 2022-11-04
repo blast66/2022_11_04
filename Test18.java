b5529344ed15e0b39d6eb9691f3aa768d72a88bc
blast66 <113650618+blast66@users.noreply.github.com>
2022-11-04 10:03:59 +0800

    Add files via upload

---
+++ Test18.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Test18 {
+    public static void main(String[] args){
+        int a;
+        a = add(7, 4);
+        System.out.println (a);
+			}
+    public static int add(int x, int y) {
+        return x + y ;
+    }
+		}
+	
